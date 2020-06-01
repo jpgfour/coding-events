f93f1001855fca71bff6f2ba1567a1c98f0bd845
jpgfour <jeremy.grise@gmail.com>
2020-06-01 15:53:46 -0500

    finished section 11.9

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -22,13 +22,15 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title","All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "CREATE EVENT");
         return "events/create";
     }
 
