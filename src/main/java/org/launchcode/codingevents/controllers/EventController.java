4c6653079572bb00adf0acfddc42198be61c93af
jpgfour <jeremy.grise@gmail.com>
2020-06-01 17:12:30 -0500

    working on exercises on new branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,12 +19,20 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events",events);
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Gencon", "The Best 4 Days In Gaming");
+        events.put("Gencant","For people who can't make it to Gencon in person, in non-pandemic years");
+        events.put("Gencon Online", "Official online version of Gencon for pandemic years");
         model.addAttribute("events",events);
+//        System.out.println(events);
+//        System.out.println(events.keySet());
+//        System.out.println(events.values());
         return "events/index";
     }
 
